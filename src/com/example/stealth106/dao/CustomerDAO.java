aa2841aeda679978caf02d150906f67df6e2e472
Bukunmi <oluwabukunmi.aluko@gmail.com>
2020-09-23 00:19:52 +0100

    Worked on the DAO

---
+++ src/com/example/stealth106/dao/CustomerDAO.java
@@ -0,0 +1,9 @@
+package com.example.stealth106.dao;
+
+import com.example.stealth106.entity.Customer;
+
+import java.util.List;
+
+public interface CustomerDAO {
+    public List<Customer> getCustomers();
+}
---
+++ src/com/example/stealth106/dao/CustomerDAOImpl.java
@@ -0,0 +1,46 @@
+package com.example.stealth106.dao;
+
+
+import java.util.List;
+
+import com.example.stealth106.entity.Customer;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Repository
+public class CustomerDAOImpl implements CustomerDAO {
+
+    // need to inject the session factory
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    @Transactional
+    public List<Customer> getCustomers() {
+
+        // get the current hibernate session
+        Session currentSession = sessionFactory.getCurrentSession();
+
+        // create a query
+        Query<Customer> theQuery =
+                currentSession.createQuery("from Customer", Customer.class);
+
+        // execute query and get result list
+        List<Customer> customers = theQuery.getResultList();
+
+        // return the results
+        return customers;
+    }
+
+}
+
+
+
+
+
+
